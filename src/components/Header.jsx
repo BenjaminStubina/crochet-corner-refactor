796f674e30305780c015c8bd2198714a701797b1
BenjaminStubina <benjamin.stubina@gmail.com>
2023-10-31 22:12:21 -0400

    Create header, start homepage body

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import basket from '../assets/icons/yarn-hook.png';
+import hook from '../assets/icons/crochet-hook-2.png';
+import { Link } from 'react-router-dom';
+
+const Header = ({ color }) => {
+
+    const headerColor = {
+        Home: 'flex py-4 bg-lightYellow justify-around',
+        US: 'flex py-4 bg-lightBlue justify-around',
+        UK: 'flex py-4 bg-lightPurple justify-around',
+        Fav: 'flex py-4 bg-lightGreen justify-around',
+        Stitch: 'flex py-4 bg-lightOrange justify-around'
+    }
+
+    return (
+        <section className={`${headerColor[color]}`}>
+            <div className='flex max-w-[320px] gap-4'>
+                <img className="h-[50px]" src={basket} alt='basket'/>
+                <Link to='/' className='flex align-middle'>
+                    <h1 className="text-[28px] self-center font-bold">
+                        Corchet Corner
+                    </h1>
+                </Link>
+                <img className="h-[50px]" src={hook} alt='hook'/>
+            </div>
+        </section>
+    )
+}
+
+export default Header
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,20 @@
+import Header from "../components/Header";
+
 const Homepage = () => {
     return (
         <>
+            <Header color='Home' />
+            <section className="flex flex-col font-bold gap-4 my-4">
+                <h2 className="flex justify-center self-center w-[280px] text-center text-[24px]">
+                    Welcome to the Crochet Corner!
+                </h2>
+                <p className="flex justify-center self-center text-[16px] w-[80%] text-center">
+                    Crochet Corner was developed as a hub for crochet enthutiast of all skill levels.
+                </p>
+                <p className="flex justify-center self-center text-[16px] w-[80%] text-center">
+                    Use the site to learn more about the different types of crochet stitches and how to differentiate between US & UK notations!
+                </p>
+            </section>
         </>
     )
 }
