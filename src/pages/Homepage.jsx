3a2af101b9f5bbaef33b24ebea812863cd3da840
BenjaminStubina <benjamin.stubina@gmail.com>
2023-11-01 01:11:46 -0400

    Create button component that changes based on props

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button, ThemeProvider, createTheme } from '@mui/material'
 import Header from "../components/Header/Header";
 import Modal from "../components/Modal/Modal";
+import NavButton from "../components/NavButton/NavButton";
 
 const Homepage = () => {
 
@@ -35,7 +36,7 @@ const Homepage = () => {
     return (
         <>
             <Header color='Home' />
-            <section className="flex flex-col font-bold gap-8 my-4">
+            <section className="flex flex-col font-bold gap-6 my-4">
                 <h2 className="flex justify-center self-center w-[280px] text-center text-[24px]">
                     Welcome to the Crochet Corner!
                 </h2>
@@ -45,11 +46,19 @@ const Homepage = () => {
                 <p className="flex justify-center self-center text-[16px] w-[80%] text-center">
                     Use the site to learn more about the different types of crochet stitches and how to differentiate between US & UK notations!
                 </p>
+                <p className="flex justify-center self-center text-[16px] w-[80%] text-center">
+                    Use the buttons below to navigate the site!
+                </p>
                 <ThemeProvider theme={theme}>
                     <Button className='flex w-[260px] self-center' variant='contained' size='medium' onClick={handleClick}>
                         About the Project
                     </Button>
                 </ThemeProvider>  
+                <div className="flex flex-col items-center gap-6">
+                    <NavButton style='US'/>
+                    <NavButton style='UK'/>
+                    <NavButton style='fav'/>
+                </div>
             </section>
             <Modal open={open} handleClose={handleClose}/>
         </>
