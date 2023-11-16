f309886880689b4a2f1e93a876c242909f4ac438
BenjaminStubina <benjamin.stubina@gmail.com>
2023-11-16 03:29:41 -0500

    Refactor folder structure

--- src/components/Tech/Tech.jsx
+++
@@ -1,7 +0,0 @@
-const Tech = ({ tech }) => {
-    return (
-        <img src={tech.image} title={tech.title} alt={tech.title} className="w-[30px] md:w-[50px] xl:w-[70px]" />
-    )
-}
-
-export default Tech;
